61fcbb7f808fa233ca831b94432d08876007af22
AtilaEzequiel <112121260+AtilaEzequiel@users.noreply.github.com>
2023-05-19 19:29:42 -0300

    Agregar archivos de proyecto.

---
+++ Controllers/PagoController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PruebaP5.Models;
+using System.Reflection;
+
+namespace PruebaP5.Controllers
+{
+    public class PagoController : Controller
+    {
+        public static List<Pago> pagoList = new List<Pago>();
+        public IActionResult Index()
+        {
+         
+
+            return View(pagoList);
+        }
+
+        
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Pago pag)
+        {
+            try
+            {
+                pagoList.Add(pag);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+
+       /* public async Task<IActionResult> Details(int cuit)
+        {
+           
+
+            //var pagos=  pagoList.Single(a => a.Cuit == cuit);
+            var pagos2 = pagoList.Single(a => a.Cuit == cuit);
+            return View(pagos2);
+        }*/
+    }
+}
---
+++ Models/Pago.cs
@@ -0,0 +1,16 @@
+﻿namespace PruebaP5.Models
+{
+    public class Pago
+    {
+        public string CondIva { get; set; }
+        // public SelectList? CondIva { get; set; }
+        // public string? PagoCondIva { get; set; }
+
+        public int Cuit { get; set; }
+
+        public string RazonSocial { get; set; }
+        public string Domicilio { get; set; }
+        public string Email { get; set; }
+        public string CondVenta { get; set; }
+    }
+}
